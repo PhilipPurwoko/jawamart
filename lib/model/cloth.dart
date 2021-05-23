3dd3852a0b2c6d132d4a8d6273c71d9bf514eb62
Philip Purwoko <philippurwoko123@gmail.com>
2021-05-23 21:26:44 +0700

    :cloud: Fix grid tile and use image network

--- lib/model/cloth.dart
+++ lib/model/cloth.dart
@@ -3,12 +3,14 @@ import 'package:flutter/foundation.dart';
 enum Size { S, M, L, XL }
 
 class Cloth {
+  final String id;
   final String name;
   final String imgUrl;
   final int price;
   final List<Size> size;
 
   Cloth({
+    @required this.id,
     @required this.name,
     @required this.imgUrl,
     @required this.price,
--- lib/screen/shop_screen.dart
+++ lib/screen/shop_screen.dart
@@ -6,14 +6,18 @@ class ShopScreen extends StatelessWidget {
 
   final List<Cloth> clothes = [
     Cloth(
+      id: '123',
       name: 'Kaos',
-      imgUrl: 'assets/images.jpg',
+      imgUrl:
+          'https://cdn.pixabay.com/photo/2014/08/26/21/48/shirts-428600_960_720.jpg',
       price: 50000,
       size: [Size.L, Size.XL],
     ),
     Cloth(
+      id: '456',
       name: 'Celana',
-      imgUrl: 'assets/images.jpg',
+      imgUrl:
+          'https://cdn.pixabay.com/photo/2016/01/19/14/45/person-1148941_960_720.jpg',
       price: 50000,
       size: [Size.S, Size.M, Size.L],
     ),
@@ -25,16 +29,37 @@ class ShopScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Jawa Mart'),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (_, index) => GridTile(
-          child: Image.asset(clothes[index].imgUrl),
-          footer: Text(clothes[index].name),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: clothes.length,
+          itemBuilder: (_, index) => ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: GridTile(
+              child: Image.network(
+                clothes[index].imgUrl,
+                fit: BoxFit.cover,
+              ),
+              footer: GridTileBar(
+                backgroundColor: Colors.black87,
+                leading: Icon(Icons.favorite),
+                trailing: Icon(Icons.shopping_cart),
+                title: Text(
+                  clothes[index].name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
