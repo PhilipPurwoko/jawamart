33e124cc9445fdcfe4a20d9ca9de07794ce852b7
Philip Purwoko <philippurwoko123@gmail.com>
2021-09-27 15:26:37 +0700

    Clean project

--- lib/provider/clothes_provider.dart
+++ lib/provider/clothes_provider.dart
@@ -35,26 +35,7 @@ class Cloth with ChangeNotifier {
 }
 
 class ClothesProvider with ChangeNotifier {
-  List<Cloth> _clothes = [
-    // Cloth(
-    //   id: '123',
-    //   name: 'Kaos',
-    //   imgUrl:
-    //       'https://cdn.pixabay.com/photo/2014/08/26/21/48/shirts-428600_960_720.jpg',
-    //   desc:
-    //       'T-shirt, atau kaos oblong, adalah model kemeja berbahan kain yang dinamai bentuk tubuh dan lengan T. Secara tradisional, ia memiliki lengan pendek dan garis leher bulat, yang dikenal sebagai leher kru, yang tidak memiliki kerah.',
-    //   price: 50000,
-    // ),
-    // Cloth(
-    //   id: '456',
-    //   name: 'Celana',
-    //   imgUrl:
-    //       'https://cdn.pixabay.com/photo/2016/01/19/14/45/person-1148941_960_720.jpg',
-    //   desc:
-    //       'Celana pendek adalah pakaian bawahan bercabang dua yang dikenakan oleh laki-laki dan perempuan di wilayah pinggul mereka, mengitari pinggang, dan menutupi bagian atas kaki, kadang-kadang lebih panjang sampai ke bawah lutut.',
-    //   price: 50000,
-    // ),
-  ];
+  List<Cloth> _clothes = [];
 
   void refresh() {
     notifyListeners();
